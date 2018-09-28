98882e2e28ff396b834a2e69d738f7e769f670e2
Zvezd04et <trikser@gmail.com>
2018-09-28 08:25:30 +0300

    refactoring

--- app/src/main/java/com/z/exercise_2/MainActivity.java
+++ app/src/main/java/com/z/exercise_2/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,35 +19,36 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout rlBase;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rlBase = findViewById(R.id.rl_base);
         addDisclaimer();
 
         final EditText messageEdit = findViewById(R.id.et_message);
         final Button emailBtn = findViewById(R.id.btn_email);
-        emailBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        emailBtn.setOnClickListener(btn -> {
 
-                final String messageString = messageEdit.getText().toString();
-                if (messageString.isEmpty()){
-                    Toast.makeText(MainActivity.this, R.string.warning_empty_message, Toast.LENGTH_LONG).show();
-                    return;
-                }
-                openEmailApp(messageString);
+            final String messageString = messageEdit.getText().toString();
+            if (messageString.isEmpty()){
+                Toast.makeText(MainActivity.this, R.string.warning_empty_message, Toast.LENGTH_LONG).show();
+                return;
             }
+            openEmailApp(messageString);
         });
 
-        final ImageView vkLogo = findViewById(R.id.iv_logo_vk);
-        vkLogo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openBrowser(getString(R.string.vk_url));
-            }
-        });
+        final ImageView vkLogo = findViewById(R.id.iv_vk_logo);
+        vkLogo.setOnClickListener(iv -> openSocialNetwork(SocialNetworks.VK));
+
+        final ImageView tmLogo = findViewById(R.id.iv_telegram_logo);
+        tmLogo.setOnClickListener(iv -> openSocialNetwork(SocialNetworks.TELEGRAM));
+
+        final ImageView gitLogo = findViewById(R.id.iv_github_logo);
+        gitLogo.setOnClickListener(iv -> openSocialNetwork(SocialNetworks.GITHUB));
 
     }
 
@@ -83,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Check if the system can handle this type of intent or startActivity will crash otherwise.
         if (intent.resolveActivity(getPackageManager()) == null) {
-            Toast.makeText(this, R.string.error_no_email_app, Toast.LENGTH_LONG).show();
+            Snackbar.make(rlBase, R.string.error_no_email_app, Snackbar.LENGTH_SHORT).show();
             return;
         }
         startActivity(intent);
@@ -92,7 +93,41 @@ public class MainActivity extends AppCompatActivity {
     private void openBrowser(@NonNull String url) {
         final Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(url));
+        // Check if the system can handle this type of intent or startActivity will crash otherwise.
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Snackbar.make(rlBase, R.string.error_no_browser_app, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
         startActivity(intent);
+    }
+
+    private boolean openSpecificApp(@NonNull String stringUrl, @NonNull String appPackage) {
+
+        Uri uri = Uri.parse(stringUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri).setPackage(appPackage);
+
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            return false;
+        }
+
+        startActivity(intent);
+        return true;
+    }
+
+    private void openSocialNetwork(@NonNull SocialNetworks socialNetwork) {
+
+        String[] appPackages = socialNetwork.getAppPackages();
+        String url = socialNetwork.getUrl();
+        if (appPackages == null) {
+            openBrowser(url);
+            return;
+        }
+
+        for (String appPackage : appPackages) {
+            if (openSpecificApp(url, appPackage)) {
+                return;}
+        }
+        openBrowser(url);
     }
 
 }
---
+++ app/src/main/java/com/z/exercise_2/SocialNetworks.java
@@ -0,0 +1,37 @@
+package com.z.exercise_2;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public enum SocialNetworks {
+
+
+    GITHUB("https://github.com/zvezd04et/"),
+    TELEGRAM("https://t.me/pantusov/", new String[]{"org.telegram.messenger", "org.thunderdog.challegram"}),
+    VK("https://vk.com/id1748403", new String[]{"com.vkontakte.android"});
+
+
+    private final String[] appPackages ;
+    private final String url;
+
+
+    SocialNetworks(@NonNull String url) {
+        this.appPackages = null;
+        this.url = url;
+    }
+
+    SocialNetworks(@NonNull String url, @NonNull String[] appPackages) {
+        this.appPackages = appPackages;
+        this.url = url;
+    }
+
+    @NonNull
+    public String[] getAppPackages() {
+        return appPackages;
+    }
+
+    @NonNull
+    public String getUrl() {
+        return url;
+    }
+}
