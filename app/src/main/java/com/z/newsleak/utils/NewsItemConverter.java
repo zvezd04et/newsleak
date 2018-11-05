191dd2a85ad37ff28722c1f54c3b52641212cd20
Zvezd04et <trikser@gmail.com>
2018-11-05 23:28:17 +0300

    refactored NewsItemConverter

--- app/src/main/java/com/z/newsleak/utils/NewsItemConverter.java
+++ app/src/main/java/com/z/newsleak/utils/NewsItemConverter.java
@@ -12,44 +12,76 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class NewsItemConverter {
 
-    public static List<NewsItem> convertFromDtos(List<NewsItemDTO> newsItemDTOs) {
+    public static List<NewsItem> convertFromDtos(@NonNull List<NewsItemDTO> newsItemDTOs, @Nullable Category currentCategory) {
 
-        List<NewsItem> news = new ArrayList<>();
+        final List<NewsItem> news = new ArrayList<>();
 
-        for (NewsItemDTO newsItemDTO:newsItemDTOs){
-            String previewImageUrl = null;
+        for (NewsItemDTO newsItemDTO : newsItemDTOs) {
+            final String previewImageUrl = getImageUrl(newsItemDTO.getMultimedia());
+            final Category category = getCategory(newsItemDTO.getSection(), currentCategory);
+            final Date publishDate = getPublishDate(newsItemDTO.getPublishedDate());
 
-            if (newsItemDTO.getMultimedia() != null && newsItemDTO.getMultimedia().size() != 0) {
-                for (ImageDTO imageDTO:newsItemDTO.getMultimedia()){
-                    if ("Normal".equals(imageDTO.getFormat())) {
-                        previewImageUrl = imageDTO.getUrl();
-                        break;
-                    }
-                }
-            }
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
-
-            Category category;
-            try {
-                category = Category.valueOf(newsItemDTO.getSection().toUpperCase());
-            } catch (IllegalArgumentException e) {
-                category = Category.HOME;
-            }
-
-            Date date;
-            try {
-                date = formatter.parse(newsItemDTO.getPublishedDate());
-                news.add(new NewsItem(newsItemDTO.getTitle(), previewImageUrl, category, newsItemDTO.getSection(),
-                        date, newsItemDTO.getAbstractField(), newsItemDTO.getUrl()));
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            news.add(new NewsItem(newsItemDTO.getTitle(), previewImageUrl, category, newsItemDTO.getSection(),
+                    publishDate, newsItemDTO.getAbstractField(), newsItemDTO.getUrl()));
         }
 
         return news;
+    }
+
+    @Nullable
+    private static String getImageUrl(@Nullable List<ImageDTO> multimedia) {
+
+        if (multimedia == null) {
+            return null;
+        }
+
+        if (multimedia.size() == 0) {
+            return null;
+        }
+
+        String previewImageUrl = null;
+        for (ImageDTO imageDTO : multimedia) {
+            if ("Normal".equals(imageDTO.getFormat())) {
+                previewImageUrl = imageDTO.getUrl();
+                break;
+            }
+        }
+        return previewImageUrl;
+    }
+
+    @NonNull
+    private static Category getCategory(@Nullable String section, @Nullable Category currentCategory) {
+
+        if (section==null){
+            return Category.HOME;
+        }
+
+        for (Category category: Category.values()){
+            if (section.equals(category.getSection())) {
+                return category;
+            }
+        }
+
+        return (currentCategory != null) ? currentCategory : Category.HOME;
+    }
+
+    @Nullable
+    private static Date getPublishDate(@Nullable String publishDate) {
+
+        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+        Date formattedDate = null;
+        try {
+            formattedDate = formatter.parse(publishDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return formattedDate;
+
     }
 
     private NewsItemConverter() {
