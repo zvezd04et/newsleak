0e5aa08f732c77d4a35bacbee2836138fafff77a
Zvezd04et <trikser@gmail.com>
2018-11-17 22:31:57 +0300

    added Delete button at NewsDetailsActivity

--- app/src/main/java/com/z/newsleak/features/news_details/NewsDetailsActivity.java
+++ app/src/main/java/com/z/newsleak/features/news_details/NewsDetailsActivity.java
@@ -3,13 +3,18 @@ package com.z.newsleak.features.news_details;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,8 +26,11 @@ import com.z.newsleak.model.NewsItem;
 import com.z.newsleak.utils.DateFormatUtils;
 import com.z.newsleak.utils.ImageLoadUtils;
 
+import java.util.concurrent.Callable;
+
 public class NewsDetailsActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "NewsDetailsActivity";
     private static final String EXTRA_NEWS_ID = "EXTRA_NEWS_ID";
 
     @NonNull
@@ -34,8 +42,12 @@ public class NewsDetailsActivity extends AppCompatActivity {
     @NonNull
     private TextView publishDateView;
 
+    @NonNull
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     @Nullable
     private Disposable disposable;
+
+    private int newsId;
 
     @NonNull
     private NewsDao database = App.getDatabase().getNewsDao();
@@ -51,7 +63,7 @@ public class NewsDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
 
-        final int newsId = getIntent().getIntExtra(EXTRA_NEWS_ID, 0);
+        newsId = getIntent().getIntExtra(EXTRA_NEWS_ID, 0);
 
         disposable = database.getNewsById(newsId)
                 .subscribeOn(Schedulers.io())
@@ -64,6 +76,45 @@ public class NewsDetailsActivity extends AppCompatActivity {
         publishDateView = findViewById(R.id.news_details_tv_publish_date);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+
+                delete();
+                return true;
+
+            default:
+                Log.d(LOG_TAG, "Selected unknown menu item");
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void delete() {
+        Disposable disposable = Completable.fromCallable((Callable<Void>) () -> {
+            database.deleteById(newsId);
+            return null;
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::onSuccessDelete, this::handleDeleteError);
+
+        compositeDisposable.add(disposable);
+    }
+
+    private void onSuccessDelete() {
+        finish();
+    }
+
+    private void handleDeleteError(Throwable throwable) {
+        Log.e(LOG_TAG, throwable.toString());
+    }
     public void showNewsDetails(NewsItem newsItem) {
         setTitle(newsItem.getCategory().getName());
 
