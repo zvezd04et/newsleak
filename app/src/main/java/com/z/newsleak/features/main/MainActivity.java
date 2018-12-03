81eb0932d13cd6b3b3039338266578c1229de9cb
Zvezd04et <trikser@gmail.com>
2018-12-03 21:21:09 +0300

    added MainActivity

---
+++ app/src/main/java/com/z/newsleak/features/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.z.newsleak.features.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.z.newsleak.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
